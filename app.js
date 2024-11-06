f99e13640384c0ca80a01143012aef27905f9f2e
kashifal <captainkashi4@gmail.com>
2024-11-07 01:57:45 +0500

    amazxing

--- app.js
+++ app.js
@@ -2,21 +2,22 @@
 const express = require('express');
 const morgan = require("morgan");
 const axios = require("axios");
+const cors = require('cors')
 const app = express();
 
 app.use(express.json());
 app.use(morgan());
-
+app.use(cors())
 
 app.get("/", async (req, res) => {
     res.json({
-        message: "Hello, world"
+        message: "Hello, world and This is updated code"
     })
 })
 
 app.post('/api/proxy/contacts', async (req, res) => {
     console.log(req.body);
-    const { email, locale, tags,calorie } = req.body;
+    const { email, locale, tags,calorie,fields } = req.body;
 
     // Validate that required fields are present
     if (!email || !locale) {
@@ -35,7 +36,8 @@ app.post('/api/proxy/contacts', async (req, res) => {
             email,
             locale: locale || 'en',
             tags:tags,
-            calorie:calorie
+            calorie:calorie,
+            fields
         }
     };
 
@@ -45,6 +47,35 @@ app.post('/api/proxy/contacts', async (req, res) => {
     } catch (error) {
         console.error("Error creating contact:", error);
         return res.status(error.response?.status || 500).json({ message: "Error creating contact", error: error.message ?? "No Error" });
+    }
+});
+
+app.post('/api/proxy/contacts/assign-tag', async (req, res) => {
+    const { userId, tagId } = req.body;
+    console.log(req)
+
+    // Validate that required fields are present
+    if (!userId || !tagId) {
+        return res.status(400).json({ message: "Please provide both userId and tagId." });
+    }
+
+    const options = {
+        method: 'POST',
+        url: `https://api.systeme.io/api/contacts/${userId}/tags`,
+        headers: {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'X-API-Key': '2gm12c714u8aev7cwp80mmfnoo6u2whvcxdl90foxccms5ouesdtri33ubexon7m'
+        },
+        data: { tagId: tagId }
+    };
+
+    try {
+        const response = await axios(options);
+        return res.json(response.data);
+    } catch (error) {
+        console.error("Error assigning tag:", error);
+        return res.status(error.response?.status || 500).json({ message: "Error assigning tag", error: error.message });
     }
 });
 app.listen(3001, () => {
